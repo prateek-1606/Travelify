4b63f1624f22a9ec35a69047e1e7e369b783ee00
varshneyprateek20@gmail.com <varshneyprateek20@gmail.com>
2022-05-01 14:35:05 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+
+function App() {
+  return (
+    <div className="App" >
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
