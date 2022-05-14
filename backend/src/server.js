23b01e22b7ec5c06251874d5c889e499453da1e5
ashish <raikwarashish1290@gmail.com>
2022-05-14 16:58:58 +0530

    user auth +model+router added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const userRouter = require('./routers/user')
-const blogRouter = require('./routers/blog')
-const User = require('.db/models/user')
-const Blog = require('./db/models/blog')
+// const blogRouter = require('./routers/blog')
+// const User = require('./db/models/user')
+// const Blog = require('./db/models/blog');
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
+
+mongoose.connect('mongodb+srv://Ashish_Raikwar:priya1234@cluster0.ja7nr.mongodb.net/Travelify?retryWrites=true&w=majority').then(() =>{
+    return app.listen({port})
+})
+.then(() => {
+    console.log('server is connected');
+})
+.catch(e => console.log(e))
 
 app.use(express.json())
 app.use(userRouter)
-app.use(blogRouter)
+// app.use(blogRouter)
 
-app.listen(port,()=>{
-    console.log('Server is up on port'+port)
-})
 
