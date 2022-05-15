e3e3a9b814dc4d325da4203f4f2baf82d45b1163
Pranil <gargshikhar82@gmail.com>
2022-05-15 15:36:14 +0530

    swswdsbjmghhj

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const userRouter = require('./routers/user')
-// const blogRouter = require('./routers/blog')
-// const User = require('./db/models/user')
-// const Blog = require('./db/models/blog');
+
 const app = express()
 const port = process.env.PORT || 3001
 
@@ -17,6 +15,3 @@ mongoose.connect('mongodb+srv://Ashish_Raikwar:priya1234@cluster0.ja7nr.mongodb.
 
 app.use(express.json())
 app.use(userRouter)
-// app.use(blogRouter)
-
-
