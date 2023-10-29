94a5472446ca4921b08ea41123c8a4e115be1dcd
-l <varshneyprateek@gmail.com>
2023-10-29 23:10:23 +0530

    Login UI Fixes

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,24 +1,18 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-    const navigate = useNavigate();
-    const user = localStorage.getItem('user');
-    useEffect(() => {
-        if (!user) {
-            navigate('/login')
-        }
-        else {
-            console.log(1);
-            navigate('/dashboard')
-        }
-    }, [])
+  const navigate = useNavigate();
+  const user = localStorage.getItem("user");
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+    } else {
+      navigate("/dashboard");
+    }
+  }, []);
 
-    return (
-        <div>
-
-        </div>
-    )
-}
+  return <div></div>;
+};
 
 export default Home;
